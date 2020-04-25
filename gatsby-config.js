95173d83c4fdfad8e904d96a573f8e0119d80408
Tejas Jadhav <tejas.jadhav@go-jek.com>
2020-04-26 03:19:37 +0530

    Update tracking ID for Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-89679997-1`,
       },
     },
     `gatsby-plugin-feed-mdx`,
